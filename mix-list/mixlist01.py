7ecac540303cae8c7e8303e42f161e907a8185e5
TheDuke-inClass <TheDuke-Sewell@users.noreply.github.com>
2021-12-14 15:38:29 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,18 @@
+#!/user/bin/env python3
+
+my_list = ["192.168.0.5", 5060, "UP"]
+
+#print the first item in the list
+print("The first item in the list (IP): " + my_list[0])
+
+#print the 2nd item in the list
+print("The second item in the list (port): " + str(my_list[1]) )
+
+
+#print the 3rd item in the list
+print("The last item in the list (state): " + my_list[2] )
+
+iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+
+print("IP address " + iplist[3] + " and " + iplist[4])
+print(f"IP address: {iplist[3]} and {iplist[4]}")
