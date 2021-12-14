07192f7333d672e5b46948552d7c571cfc62df2d
TheDuke-inClass <TheDuke-Sewell@users.noreply.github.com>
2021-12-14 19:42:25 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def main():
+
+ #create a list called animals
+ animals1 = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat"]
+ animals1.extend(["Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"])
+
+ #display the list
+ print(animals1)
+
+ #sort the list
+ animals1.sort()
+
+ print(animals1)
+
+main()
